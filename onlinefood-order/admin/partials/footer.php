62bc0fb1aa40493feed6b622d55ea38161cda67b
4chalstore <106409319+HSmovies@users.noreply.github.com>
2025-03-07 19:26:15 -0800

    Add files via upload

---
+++ onlinefood-order/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"><?php echo date("Y"); ?> 4chal store هەموو مافێکی پارێزراوە بۆ ماڵپەری </a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
---
+++ onlinefood-order/partials-front/footer.php
@@ -0,0 +1,26 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://wa.me/+9647503526163"><img src="https://img.icons8.com/fluent/40/000000/whatsapp.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/4chalstore?igsh=MzRlODBiNWFlZA=="><img src="https://img.icons8.com/fluent/38/000000/instagram-new.png"/></a>
+                </li>
+               
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>4chal store هەموو مافێکی پارێزراوە بۆ ماڵپەری </p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
